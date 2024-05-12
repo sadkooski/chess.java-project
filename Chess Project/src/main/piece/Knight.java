d293d8a278a07f6038cd443d85690480e408072d
Oskar Sadkowski <117320665+sadkooski@users.noreply.github.com>
2024-05-12 19:41:31 +0200

    Knight's movement logic

--- Chess Project/src/main/piece/Knight.java
+++ Chess Project/src/main/piece/Knight.java
@@ -13,5 +13,17 @@ public class Knight extends Piece{
             image = getImage("/res/piece/b_knight") ;
         }
     }
+    public boolean canMove(int targetCol, int targetRow) {
+
+        if(isWithinBoard(targetCol, targetRow)) {
+            /// Goniec może się poruszać tylko jeśli jego movement ratio w kolumnnie i wierszu planszy wynosi 1:2 albo 2:1
+            if(Math.abs(targetCol - preCol) * Math.abs(targetRow - preRow) == 2) {
+                if(isValidSquare(targetCol, targetRow)) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
         
 }
